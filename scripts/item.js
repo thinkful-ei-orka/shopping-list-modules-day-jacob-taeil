81e2373829a229667b0666d262911e5ca2ceb019
Taeil Kwak <taeil2@gmail.com>
2020-04-27 13:36:09 -0500

    added functions to the item module

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,20 @@
-export default {};
+const validateName = function(name) {
+  if (name === '') {
+    throw new TypeError('Name must not be blank');
+  }
+};
+
+const create = function(name) {
+  let item = {
+    id: cuid(),
+    name: name,
+    checked: false
+  };
+
+  return item;
+};
+
+export default {
+  validateName,
+  create
+};
